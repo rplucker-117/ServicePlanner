9840ebb49d4d157b214fb6ebe2d6c0ffcdc2130f
rplucker-117 <rplucker@thelife.cc>
2021-05-05 10:33:32 -0500

    added ip / port variables to class init

--- pvp.py
+++ pvp.py
@@ -5,9 +5,13 @@ from settings import *
 import logging
 
 class pvp:
-    def get_pvp_data(*self, ip, port):
-        r = requests.get(f'http://{ip}:{port}/api/0/data/playlists')
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+    def get_pvp_data(self):
+        r = requests.get(f'http://{self.ip}:{self.port}/api/0/data/playlists')
         data = json.loads(r.text)
         return data
-    def cue_clip(*self, ip, port, playlist, clip_number):
-        requests.post(f"http://{ip}:{port}/api/0/trigger/playlist/{playlist}/cue/{clip_number}")
+    def cue_clip(self, playlist, clip_number):
+        requests.post(f"http://{self.ip}:{self.port}/api/0/trigger/playlist/{playlist}/cue/{clip_number}")
