6e70ccd29c8a88954e7110b4cd0c4938ee0d3270
rplucker-117 <rplucker@thelife.cc>
2021-05-13 15:39:55 -0500

    Changed cue_clip to cue_clip via index Added ability to cue a clip based on uuid

--- pvp.py
+++ pvp.py
@@ -13,5 +13,12 @@ class pvp:
         r = requests.get(f'http://{self.ip}:{self.port}/api/0/data/playlists')
         data = json.loads(r.text)
         return data
-    def cue_clip(self, playlist, clip_number):
+    def cue_clip_via_index(self, playlist, clip_number):
         requests.post(f"http://{self.ip}:{self.port}/api/0/trigger/playlist/{playlist}/cue/{clip_number}")
+
+    def cue_clip_via_uuid(self, uuid):
+        requests.post(f"http://{self.ip}:{self.port}/api/0/trigger/cue/{uuid}")
+
+if __name__ == '__main__':
+    pvp = pvp(ip='10.1.60.95', port=49868)
+    pprint.pprint(pvp.get_pvp_data())
