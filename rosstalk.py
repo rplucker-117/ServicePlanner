86d269ff4beb9607ecfb8bfefb1eb911400bc3b2
rplucker-117 <rplucker@thelife.cc>
2021-05-13 20:21:32 -0500

    Port now accepts strings and ints

--- rosstalk.py
+++ rosstalk.py
@@ -2,7 +2,7 @@ import socket
 
 def rosstalk(rosstalk_ip, rosstalk_port, command):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((rosstalk_ip, rosstalk_port))
+    s.connect((rosstalk_ip, int(rosstalk_port)))
     s.send(bytes(command, 'utf-8'))
     s.send(b'\r\n')
     s.close()
