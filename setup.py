0c28dc429aab9487bbcd871c31db07cce7e7c909
rplucker-117 <rplucker@thelife.cc>
2024-05-28 16:35:30 -0500

    Added all dependancies to setup list

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
 
-packages = ['logzero', 'requests', 'zulu', 'demjson', 'pymidi', 'wget', 'Flask', 'ezodf', 'lxml', 'flask_socketio', 'gevent-websocket', 'beautifulsoup4', 'rtmidi2']
+packages = ['Babel', 'Cython', 'Flask', 'Flask-SocketIO', 'MarkupSafe', 'Pillow', 'Werkzeug', 'beautifulsoup4', 'bidict', 'certifi', 'cffi', 'charset-normalizer', 'click', 'colorama', 'construct', 'ezodf', 'future', 'gevent', 'gevent-websocket', 'greenlet', 'h11', 'idna', 'importlib-metadata', 'iso8601', 'itsdangerous', 'jinja2', 'logzero', 'lxml', 'pip', 'pycparser', 'pymidi', 'python-dateutil', 'python-engineio', 'python-socketio', 'pytimeparse', 'pytz', 'requests', 'rtmidi2', 'setuptools', 'simple-websocket', 'six', 'soupsieve', 'typing-extensions', 'urllib3', 'wget', 'wheel', 'wsproto', 'zipp', 'zope.event', 'zope.interface', 'zulu']
 
 
 def update_pip():
