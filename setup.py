c5b692fc9bfefcca18ff5c985725be96c6c9168d
rplucker-117 <rplucker@thelife.cc>
2024-07-25 11:36:42 -0500

    Add additional dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,59 @@
 import subprocess
 import sys
 
-packages = ['Babel', 'Cython', 'Flask', 'Flask-SocketIO', 'MarkupSafe', 'Pillow', 'Werkzeug', 'beautifulsoup4', 'bidict', 'certifi', 'cffi', 'charset-normalizer', 'click', 'colorama', 'construct', 'ezodf', 'future', 'gevent', 'gevent-websocket', 'greenlet', 'h11', 'idna', 'importlib-metadata', 'iso8601', 'itsdangerous', 'jinja2', 'logzero', 'lxml', 'pip', 'pycparser', 'pymidi', 'python-dateutil', 'python-engineio', 'python-socketio', 'pytimeparse', 'pytz', 'requests', 'rtmidi2', 'setuptools', 'simple-websocket', 'six', 'soupsieve', 'typing-extensions', 'urllib3', 'wget', 'wheel', 'wsproto', 'zipp', 'zope.event', 'zope.interface', 'zulu']
+packages = ['Babel',
+            'bscpylgtv',
+            'Cython',
+            'Flask',
+            'Flask-SocketIO',
+            'MarkupSafe',
+            'Pillow',
+            'Werkzeug',
+            'beautifulsoup4',
+            'bidict',
+            'certifi',
+            'cffi',
+            'charset-normalizer',
+            'click',
+            'colorama',
+            'construct',
+            'ezodf',
+            'future',
+            'gevent',
+            'gevent-websocket',
+            'greenlet',
+            'h11',
+            'idna',
+            'importlib-metadata',
+            'iso8601',
+            'itsdangerous',
+            'jinja2',
+            'logzero',
+            'lxml',
+            'pip',
+            'pycparser',
+            'pymidi',
+            'python-dateutil',
+            'python-engineio',
+            'python-socketio',
+            'pytimeparse',
+            'pytz',
+            'requests',
+            'rtmidi2',
+            'setuptools',
+            'simple-websocket',
+            'six',
+            'soupsieve',
+            'typing-extensions',
+            'urllib3',
+            'wakeonlan'
+            'wget',
+            'wheel',
+            'wsproto',
+            'zipp',
+            'zope.event',
+            'zope.interface',
+            'zulu']
 
 
 def update_pip():
@@ -11,6 +63,7 @@ def update_pip():
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-
-for package in packages:
-    install(package)
+if __name__ == '__main__':
+    update_pip()
+    for package in packages:
+        install(package)
