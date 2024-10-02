0279bbac041e241d1e23eca587040a4318c47f8a
rplucker-117 <rplucker@thelife.cc>
2024-10-02 12:13:17 -0500

    Logging formatting changes

--- pvp.py
+++ pvp.py
@@ -5,13 +5,14 @@ from logzero import logger
 import urllib3.exceptions
 from typing import Union, List, Dict
 
+
 class PVP:
     def __init__(self, ip: str, port: int):
         self.ip = ip
         self.port = port
         self.API_BASE_URL = f'http://{self.ip}:{self.port}/api/0'
 
-    def _make_get_request(self, endpoint: str) -> Union[Dict, int]:
+    def _make_get_request(self, endpoint: str) -> Dict | int | None:
         """
         Make a GET request to a specified endpoint
         :param endpoint: The endpoint to make a request to, exactly as it's listed in the documentation including the leading /.
@@ -20,7 +21,8 @@ class PVP:
 
         request_url = f'{self.API_BASE_URL}{endpoint}'
 
-        logger.debug(f'{__class__.__name__}.{self._make_get_request.__name__}: Making get request to endpoint {request_url}.')
+        logger.debug(
+            f'{__class__.__name__}.{self._make_get_request.__name__}: Making get request to endpoint {request_url}.')
 
         try:
             r = requests.get(request_url)
@@ -34,7 +36,9 @@ class PVP:
                 requests.exceptions.ConnectionError,
                 urllib3.exceptions.NewConnectionError,
                 ConnectionRefusedError):
-            logger.warning(f'{__class__.__name__}.{self._make_get_request.__name__}: PVP machine at {self.ip}:{self.port} is either offline, PVP is not running, or the Network API is not configured correctly.')
+            logger.warning(
+                f'{__class__.__name__}.{self._make_get_request.__name__}: PVP machine at {self.ip}:{self.port} is either offline, PVP is not running, or the Network API is not configured correctly.')
+            return None
 
     def _make_post_request(self, endpoint: str) -> None:
         """
@@ -45,7 +49,8 @@ class PVP:
 
         request_url = f'{self.API_BASE_URL}{endpoint}'
 
-        logger.debug(f'{__class__.__name__}.{self._make_post_request.__name__}: Making post request to endpoint {request_url}.')
+        logger.debug(
+            f'{__class__.__name__}.{self._make_post_request.__name__}: Making post request to endpoint {request_url}.')
 
         try:
             requests.post(request_url)
@@ -54,10 +59,10 @@ class PVP:
                 requests.exceptions.ConnectionError,
                 urllib3.exceptions.NewConnectionError,
                 ConnectionRefusedError):
-            logger.warning(f'{__class__.__name__}.{self._make_post_request.__name__}: PVP machine at {self.ip}:{self.port} is either offline, PVP is not running, or the Network API is not configured correctly.')
+            logger.warning(
+                f'{__class__.__name__}.{self._make_post_request.__name__}: PVP machine at {self.ip}:{self.port} is either offline, PVP is not running, or the Network API is not configured correctly.')
 
-
-    def get_pvp_playlists(self) -> dict:
+    def get_pvp_playlists(self) -> dict | None:
         """
         Get all playlists.
         :return: All playlists.
@@ -65,7 +70,7 @@ class PVP:
         logger.debug(f'{__class__.__name__}.{self.get_pvp_playlists.__name__}: Getting playlists for {self.ip}:{self.port}')
         return self._make_get_request('/data/playlists')
 
-    def get_pvp_layers(self) -> dict:
+    def get_pvp_layers(self) -> dict | None:
         """
         Get all layers
         :return: All layers.
@@ -98,14 +103,14 @@ class PVP:
         :return: bool of result
         """
 
-        logger.debug(f'{__class__.__name__}.{self.does_cue_exist.__name__}: Checking to see if cue exists on machine {self.ip}:{self.port}. playlist_uuid: {playlist_uuid}, cue_uuid: {cue_uuid}')
+        logger.debug(
+            f'{__class__.__name__}.{self.does_cue_exist.__name__}: Checking to see if cue exists on machine {self.ip}:{self.port}. playlist_uuid: {playlist_uuid}, cue_uuid: {cue_uuid}')
         data = self._make_get_request(endpoint=f'/data/playlist/{playlist_uuid}/cue/{cue_uuid}')
 
         if data == 400:  # method returned status code 400 because cue does not exist
             return False
         if type(data) is dict:  # method returned type dict because cue exists
             return True
-
 
     def cue_clip(self, playlist: str, cue: str) -> None:
         """
@@ -115,7 +120,8 @@ class PVP:
         :return: None.
         """
 
-        logger.debug(f'{__class__.__name__}.{self.cue_clip.__name__}: Triggering cue {cue} in playlist {playlist} on machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.cue_clip.__name__}: Triggering cue {cue} in playlist {playlist} on machine {self.ip}:{self.port}')
 
         self._make_post_request(f'/trigger/playlist/{playlist}/cue/{cue}')
 
@@ -124,7 +130,8 @@ class PVP:
         Clears entire workspace.
         :return: None.
         """
-        logger.debug(f'{__class__.__name__}.{self.clear_workspace.__name__}: Clearing workspace on machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.clear_workspace.__name__}: Clearing workspace on machine {self.ip}:{self.port}')
 
         self._make_post_request('/clear/workspace')
 
@@ -135,7 +142,8 @@ class PVP:
         :return: None.
         """
 
-        logger.debug(f'{__class__.__name__}.{self.clear_layer.__name__}: Clearing layer {layer_id} on machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.clear_layer.__name__}: Clearing layer {layer_id} on machine {self.ip}:{self.port}')
 
         self._make_post_request(f'/clear/layer/{layer_id}')
 
@@ -145,7 +153,8 @@ class PVP:
         :return: None.
         """
 
-        logger.debug(f'{__class__.__name__}.{self.mute_workspace.__name__}: Muting workspace on pvp machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.mute_workspace.__name__}: Muting workspace on pvp machine {self.ip}:{self.port}')
 
         self._make_post_request(f'/mute/workspace')
 
@@ -155,7 +164,8 @@ class PVP:
         :return: None.
         """
 
-        logger.debug(f'{__class__.__name__}.{self.unmute_workspace.__name__}: Unmuting workspace on pvp machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.unmute_workspace.__name__}: Unmuting workspace on pvp machine {self.ip}:{self.port}')
         self._make_post_request(f'/unmute/workspace')
 
     def mute_layer(self, layer_id) -> None:
@@ -165,7 +175,8 @@ class PVP:
         :return: None
         """
 
-        logger.debug(f'{__class__.__name__}.{self.mute_layer.__name__}: Muting layer {layer_id} on pvp machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.mute_layer.__name__}: Muting layer {layer_id} on pvp machine {self.ip}:{self.port}')
         self._make_post_request(f'/mute/layer/{layer_id}')
 
     def unmute_layer(self, layer_id) -> None:
@@ -175,7 +186,8 @@ class PVP:
         :return:
         """
 
-        logger.debug(f'{__class__.__name__}.{self.mute_layer.__name__}: Unmuting layer {layer_id} on pvp machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.mute_layer.__name__}: Unmuting layer {layer_id} on pvp machine {self.ip}:{self.port}')
         self._make_post_request(f'/unmute/layer/{layer_id}')
 
     def hide_workspace(self) -> None:
@@ -184,7 +196,8 @@ class PVP:
         :return: None
         """
 
-        logger.debug(f'{__class__.__name__}.{self.hide_workspace.__name__}: Hiding workspace on pvp machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.hide_workspace.__name__}: Hiding workspace on pvp machine {self.ip}:{self.port}')
         self._make_post_request(f'/hide/workspace')
 
     def unhide_workspace(self) -> None:
@@ -193,7 +206,8 @@ class PVP:
         :return: None
         """
 
-        logger.debug(f'{__class__.__name__}.{self.unhide_workspace.__name__}: Unhiding workspace on pvp machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.unhide_workspace.__name__}: Unhiding workspace on pvp machine {self.ip}:{self.port}')
         self._make_post_request(f'/unhide/workspace')
 
     def hide_layer(self, layer_id) -> None:
@@ -202,7 +216,8 @@ class PVP:
         :param layer_id: Can be uuid, name, or index (integer) of the layer that we want to hide. Number only parameters are always interpreted as an index and never name.
         :return: None
         """
-        logger.debug(f'{__class__.__name__}.{self.hide_layer.__name__}: Hiding layer {layer_id} on pvp machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.hide_layer.__name__}: Hiding layer {layer_id} on pvp machine {self.ip}:{self.port}')
         self._make_post_request(f'/hide/layer/{layer_id}')
 
     def unhide_layer(self, layer_id) -> None:
@@ -212,7 +227,8 @@ class PVP:
         :return: None
         """
 
-        logger.debug(f'{__class__.__name__}.{self.hide_layer.__name__}: Unhiding layer {layer_id} on pvp machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.hide_layer.__name__}: Unhiding layer {layer_id} on pvp machine {self.ip}:{self.port}')
         self._make_post_request(f'/unhide/layer/{layer_id}')
 
     def pause_workspace(self) -> None:
@@ -221,7 +237,8 @@ class PVP:
         :return: None
         """
 
-        logger.debug(f'{__class__.__name__}.{self.pause_workspace.__name__}: Pausing workspace on pvp machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.pause_workspace.__name__}: Pausing workspace on pvp machine {self.ip}:{self.port}')
         self._make_post_request(f'/pause/workspace')
 
     def unpause_workspace(self) -> None:
@@ -230,7 +247,8 @@ class PVP:
         :return: None
         """
 
-        logger.debug(f'{__class__.__name__}.{self.unpause_workspace.__name__}: Unpausing workspace on pvp machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.unpause_workspace.__name__}: Unpausing workspace on pvp machine {self.ip}:{self.port}')
         self._make_post_request(f'/play/workspace')
 
     def pause_layer(self, layer_id) -> None:
@@ -240,7 +258,8 @@ class PVP:
         :return: None
         """
 
-        logger.debug(f'{__class__.__name__}.{self.pause_layer.__name__}: Pausing layer {layer_id} on pvp machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.pause_layer.__name__}: Pausing layer {layer_id} on pvp machine {self.ip}:{self.port}')
         self._make_post_request(f'/pause/layer/{layer_id}')
 
     def unpause_layer(self, layer_id) -> None:
@@ -261,8 +280,12 @@ class PVP:
         :return: string of the name of the cue
         """
 
-        logger.debug(f'{__class__.__name__}.{self.cue_name_from_uuids.__name__}: Getting clip name from playlist id {playlist_uuid}, cue uuid {cue_uuid} on machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.cue_name_from_uuids.__name__}: Getting clip name from playlist id {playlist_uuid}, cue uuid {cue_uuid} on machine {self.ip}:{self.port}')
         playlists = self.get_pvp_playlists()
+
+        if playlists is None:
+            return '[device offline]'
 
         for playlist in playlists['playlist']['children']:
             if playlist['uuid'] == playlist_uuid:
@@ -278,7 +301,8 @@ class PVP:
         :param layer_uuid: uuid of layer
         :return: string of the name
         """
-        logger.debug(f'{__class__.__name__}.{self.get_layer_name_from_uuid.__name__}: Getting layer name from uuid {layer_uuid} on machine {self.ip}:{self.port}')
+        logger.debug(
+            f'{__class__.__name__}.{self.get_layer_name_from_uuid.__name__}: Getting layer name from uuid {layer_uuid} on machine {self.ip}:{self.port}')
 
         layer = self._make_get_request(f'/data/layer/{layer_uuid}')
 
@@ -292,5 +316,4 @@ if __name__ == '__main__':
     pvp = PVP(ip='10.1.60.91', port=49343)
     # print(pvp.cue_name_from_uuids(playlist_uuid='FDE14BF5-22BB-4499-8EFB-373C0743B15E', cue_uuid='3A173EFA-50FD-4705-9ABB-E9CDC9F96CC5'))
     pvp.get_layer_name_from_uuid(0)
-#Transition video: playlist: FDE14BF5-22BB-4499-8EFB-373C0743B15E, cue: 3A173EFA-50FD-4705-9ABB-E9CDC9F96CC5
-
+# Transition video: playlist: FDE14BF5-22BB-4499-8EFB-373C0743B15E, cue: 3A173EFA-50FD-4705-9ABB-E9CDC9F96CC5
